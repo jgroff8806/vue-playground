d6140ce325100f81353aee29a39f13835ee55566
Jason <jgroff88@gmail.com>
2017-12-21 23:05:50 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <h1>Jason</h1>
+    <p>This is a dong paragraph</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+  },
+  data: {
+    show: true,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
